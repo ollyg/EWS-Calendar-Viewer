f4428fe84a9a0805b9ebf36da20aa3a9f9cac552
Oliver Gorwits <oliver@cpan.org>
2010-02-22 14:30:22 +0000

    do not show secret data when debug error screen is shown

--- lib/EWS/Calendar/Viewer.pm
+++ lib/EWS/Calendar/Viewer.pm
@@ -3,6 +3,7 @@ package EWS::Calendar::Viewer;
 use strict;
 use warnings FATAL => 'all';
 
+use MRO::Compat;
 use Catalyst qw/
     -Debug
     ConfigLoader
@@ -12,6 +13,14 @@ use Catalyst qw/
 
 our $VERSION = '0.01';
 $VERSION = eval $VERSION; # numify for warning-free dev releases
+
+# skip s3krits from dumped data
+sub dump_these {
+    my $c = shift;
+    my @variables = $c->next::method(@_);
+    return grep { $_->[0] !~ m/^(?:Config|Response|Stash)$/ } @variables;
+}
+
 
 __PACKAGE__->setup;
 
