e4be9b2dd4ed8cf655d11cb6d3dd9bca9eb9e226
Oliver Gorwits <oliver@cpan.org>
2010-02-22 16:00:38 +0000

    rename privacy setting to privacy_level and move to stash

--- lib/EWS/Calendar/Viewer/Controller/Root.pm
+++ lib/EWS/Calendar/Viewer/Controller/Root.pm
@@ -27,6 +27,7 @@ sub base : Chained('/') PathPart('') CaptureArgs(0) {
     my( $self, $c )  = @_;
     $c->stash->{now} = DateTime->now->set( day => 1 );
     $c->stash->{version} = $EWS::Calendar::Viewer::VERSION;
+    $c->stash->{privacy_level} = ($c->config->{privacy_level} || 'public');
 
     my @days = (qw/ Sunday Monday Tuesday Wednesday Thursday Friday Saturday /);
     my $sow = $c->config->{start_of_week};
