75f3fe90843ef9d05aa92b0a85dd65aadffc3a9d
Oliver Gorwits <oliver@cpan.org>
2010-02-23 00:02:11 +0000

    add packaging and POD

--- lib/EWS/Calendar/Viewer.pm
+++ lib/EWS/Calendar/Viewer.pm
@@ -21,16 +21,17 @@ sub dump_these {
 }
 
 __PACKAGE__->config({
-    name => 'EWS Calendar Viewer',
+    name => 'EWS::Calendar::Viewer',
 });
 
 __PACKAGE__->setup;
 
 1;
 __END__
+
 =head1 NAME
 
-EWS::Calendar::Viewer - Web viewer for your MS Exchange calendar
+EWS::Calendar::Viewer - Share your MS Exchange Calendar via a Web Page
 
 =head1 VERSION
 
@@ -38,110 +39,64 @@ This document refers to version 0.01 of EWS::Calendar::Viewer
 
 =head1 SYNOPSIS
 
+I recommend you use something like L<App::BundleDeps> to deploy this under a
+fastcgi server environment. Configure the application like so:
 
-
-Set up your Exchange Web Services client:
-
- use EWS::Client;
- use DateTime;
+ privacy_level = limited
+ start_of_week = 1
  
- my $ews = EWS::Client->new({
-     server      => 'exchangeserver.example.com',
-     username    => 'oliver',
-     password    => 's3krit', # or set in $ENV{EWS_PASS}
- });
+ <Model::EWSClient>
+     <args>
+         server   = myserver.example.com
+         username = oliver
+     </args>
+ </Model::EWSClient>
 
-Then perform operations on the Exchange server:
+And then start the Catalyst applicaton, perhaps using one of the bundled
+server scripts.
 
- my $entries = $ews->calendar->retrieve({
-     start => DateTime->now(),
-     end   => DateTime->now->add( month => 1 ),
- });
- 
- print "I retrieved ". $entries->count ." items\n";
- 
- while ($entries->has_next) {
-     print $entries->next->Subject, "\n";
- }
+=head1 CONFIGURTATION
 
-=head1 DESCRIPTION
+=head2 privacy_level
 
-This module acts as a client to the Microsoft Exchange Web Services API. From
-here you can access calendar (and soon, contact) entries in a nicely
-abstracted fashion. Query results are generally available in an iterator and
-convenience methods exist to access the properties of each entry.
+This can be set to C<public> to show only your free/busy status, C<limited> to
+show the title of the event as well, or C<private> to show all details of the
+event in a tooltip.
 
-=head1 METHODS
+=head2 start_of_week
 
-=head2 CONSTRUCTOR
+Set this to a number from 0 to 6 representing Sunday through to Saturday
+respectively.
 
-=head2 EWS::Client->new( \%arguments )
+=head2 EWS Client
 
-Instantiates a new EWS client. There won't be any connection to the server
-until you call one of the calendar or contacts retrieval methods.
-
-=over 4
-
-=item C<server> => Fully Qualified Domain Name (required)
-
-The host name of the Exchange server to which the module should connect.
-
-=item C<username> => String (required)
-
-The account username under which the module will connect to Exchange. This
-value will be URI encoded by the module.
-
-=item C<password> => String OR via C<$ENV{EWS_PASS}> (required)
-
-The password of the account under which the module will connect to Exchange.
-This value will be URI encoded by the module. You can also provide the
-password via the C<EWS_PASS> environment variable.
-
-=item C<schema_path> => String (optional)
-
-A folder on your file system which contains the WSDL and two further Schema
-files (messages, and types) which describe the Exchange 2007 Web Services SOAP
-API. They are shipped with this module so your providing this is optional.
-
-=back
-
-=head2 COMPONENT METHODS
-
-There exist separate components to access the calendar (and soon, contacts)
-entries on the Exchange server, each bundled with this module but having its
-own manual page. Each component is accessed through a method and follow the
-links below to learn about the available features.
-
-=head2 $ews->calendar()
-
-Retrieves the L<EWS::Client::Calendar> object which allows search and
-retrieval of calendar entries.
-
-=head1 TODO
-
-I might look at moving away from L<XML::Compile::SOAP>, which is truely the
-most awesome of modules, but overkill for ths very small set of static calls.
-
-There is currently no handling of time zone information whatsoever. I'm
-waiting for my timezone to shift to UTC+1 in March before working on this, as
-I don't really want to read the Exchange API docs. Patches are welcome if you
-want to help out.
+You'll need to set the server fully qualified domain name, and username for
+the calendar's account. The password can be set in the file using the
+C<password> option or via the environment variable C<EWS_PASS>.
 
 =head1 REQUIREMENTS
 
 =over 4
 
-=item * L<Moose>
+=item * L<Catalyst::Runtime> >= 5.8002
 
-=item * L<XML::Compile::SOAP>
+=item * L<Catalyst::Plugin::ConfigLoader>
+
+=item * L<Catalyst::Plugin::Static::Simple>
+
+=item * L<Catalyst::Action::RenderView>
+
+=item * L<Catalyst::Model::Adaptor>
+
+=item * L<Catalyst::View::TT>
+
+=item * L<Config::General>
+
+=item * L<Calendar::Simple>
 
 =item * L<DateTime>
 
-=item * L<DateTime::Format::ISO8601>
-
-=item * L<HTML::Strip>
-
-=item * L<URI::Escape>
+=item * L<MRO::Compat>
 
 =back
 
